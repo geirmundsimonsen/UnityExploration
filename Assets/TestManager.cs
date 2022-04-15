538381393c133ab59ccf9690860280d132e15501
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2022-04-15 12:29:46 +0200

    Add TestManager which should have responsibility for end-to-end testing.

---
+++ Assets/TestManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
