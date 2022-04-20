e8bfdbadedb4984013862321b3a2a9d0e2a92890
Geirmund Simonsen <geirmund.simonsen@gmail.com>
2022-04-20 16:57:28 +0200

    Stop using custom Debug

--- Assets/Scripts/Types.cs
+++ Assets/Scripts/Types.cs
@@ -15,14 +15,14 @@ public class EnemyStats {
     public bool damageOnContact;
 }
 
-public class Debug {
+/*public class Debug {
     static System.DateTime timeAtBeginning = System.DateTime.Now;
     public static void Log(object obj) {
         System.TimeSpan ts = (System.DateTime.Now - timeAtBeginning);
         string time = string.Format("{0:N3}", ts.TotalSeconds);
         UnityEngine.Debug.Log(time + ": " + obj);
     }
-}
+}*/
 
 public class Value {
     public float value;
